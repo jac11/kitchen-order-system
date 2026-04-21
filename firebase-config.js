0d467e4b69f82e999fa39a6bb117dba3e47acfc2
jac11 <jac11devel@gmail.com>
2026-04-20 21:43:37 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7xF2kP5jgH6xyGg7-S-T_QgMoBDIoBBc",
+  authDomain: "kitchenordersystem-66eb2.firebaseapp.com",
+  projectId: "kitchenordersystem-66eb2",
+  storageBucket: "kitchenordersystem-66eb2.firebasestorage.app",
+  messagingSenderId: "786074432195",
+  appId: "1:786074432195:web:8381f2cba8f43002232876"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
